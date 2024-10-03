9b04e18bbc834d296463720125e2aa1a7f643871
Hrithik Agarwal <hrithikinfinite@gmail.com>
2024-10-03 19:29:03 +0530

    second stage

--- app/main.js
+++ app/main.js
@@ -6,6 +6,9 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
+  const coorelationID = 7;
+  const response = Buffer.from([0, 0, 0, 0, 0, 0, 0, coorelationID]);
+  connection.write(response);
 });
 
 server.listen(9092, "127.0.0.1");
